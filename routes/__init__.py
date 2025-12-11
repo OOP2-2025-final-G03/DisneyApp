36d5c2a3978479fec081477290d06b0b58cbd1d0
告白 <kout5504@gmail.com>
2025-12-11 16:51:22 +0900

    Update __init__.py

--- routes/__init__.py
+++ routes/__init__.py
@@ -3,7 +3,6 @@ from .product import product_bp
 from .history import history_bp
 from .order import order_bp
 from .attraction import attraction_bp
-from .history import history_bp
 from .area import area_bp
 
 # Blueprintをリストとしてまとめる
@@ -13,6 +12,5 @@ blueprints = [
   history_bp,
   order_bp,
   attraction_bp,
-  history_bp,
   area_bp #area_Blueprintの追加
 ]
